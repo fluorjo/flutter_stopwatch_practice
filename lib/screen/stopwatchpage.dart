1506689fbcc67bd49fb60ae60a591d1392eadebb
fluorjo <fluorjo@naver.com>
2023-09-17 15:04:13 +0900

    ui-시작/일시정지 버튼 영역

---
+++ lib/screen/stopwatchpage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class StopWatchPage extends StatefulWidget {
+  const StopWatchPage({super.key});
+
+  @override
+  State<StopWatchPage> createState() => _StopWatchPageState();
+}
+
+class _StopWatchPageState extends State<StopWatchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('StopWatch'),
+      ),
+      body: _buildBody(),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(
+          () {
+            _clickButton();
+          },
+        ),
+        child: const Icon(
+          Icons.play_arrow,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
